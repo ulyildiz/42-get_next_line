58a6d6131861408045bdaf703d43db076f5b23fb
Ulas Berke Yildiz <ulyildiz@k2m02s05.42kocaeli.com.tr>
2023-11-01 10:42:37 +0300

    f

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ulyildiz <ulyildiz@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 16:43:46 by ulyildiz          #+#    #+#             */
-/*   Updated: 2023/11/01 10:08:41 by ulyildiz         ###   ########.fr       */
+/*   Updated: 2023/11/01 10:38:30 by ulyildiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
-#include <get_next_line.h>
+#include "get_next_line.h"
 
 static char	*ft_read_line(int fd)
 {
-	int	i;
+	int			i;
 	char	*rl;
 
 	rl = (char *)malloc(BUFFER_SIZE + 1);
-	i = read(fd, rl, BUFFER_SIZE + 1);
+	if (rl == NULL)
+		return (NULL);
+	rl[BUFFER_SIZE] = '\0';
+	i = read(fd, rl, BUFFER_SIZE);
 	if (i == -1)
 	{
 		free(rl);
@@ -33,14 +36,29 @@ static char	*ft_read_line(int fd)
 	return (rl);
 }
 
-void	ft_catch_newline(char *keep)
+static int	ft_catch_newline(char *line, char *wwn)
 {
-	
+	int	i;
+
+	i = 0;
+	wwn[BUFFER_SIZE] = '\0';
+	while (line[i] != '\n' && line[i] != '\0')
+	{
+		wwn[i] = line[i];
+		i++;
+	}
+	if (line[i] == '\n')
+		wwn[i] = '\n';
+	else if (line[i] == '\0')
+		wwn[i] = '\0';
+	free(line);
+	return (i);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*line;
+	char	*line;
+	char	*wwn;
 	
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -53,5 +71,12 @@ char	*get_next_line(int fd)
 		free(line);
 		return (NULL);
 	}
-	
+	wwn = (char *)malloc(BUFFER_SIZE + 1);
+	if (wwn == NULL)
+	{
+		free(line);
+		return (NULL);
+	}		
+	ft_catch_newline(line, wwn);
+	return (wwn);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,17 @@
 /*   By: ulyildiz <ulyildiz@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 16:43:42 by ulyildiz          #+#    #+#             */
-/*   Updated: 2023/10/31 20:27:34 by ulyildiz         ###   ########.fr       */
+/*   Updated: 2023/11/01 10:24:26 by ulyildiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+#ifndef BUFFER_SIZE
 # define BUFFER_SIZE 4
+#endif
 
 char	*get_next_line(int fd);
+
 
 #endif
